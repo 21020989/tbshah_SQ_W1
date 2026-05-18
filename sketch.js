573c6d32ded5ea12b8242f73e1f01da21aceb3f1
21020989 <tbshah@uwaterloo.ca>
2026-05-18 17:38:14 -0400

    added text

--- sketch.js
+++ sketch.js
@@ -7,6 +7,13 @@ function preload() {
 function setup() {
   createCanvas(800, 600);
   background(bgImage);
+  noStroke();
+  rect(0, 0, 800, 100);
+  textSize(32);
+  textAlign(CENTER, TOP);
+  fill(0);
+  noStroke();
+  text("Find the 8 Differences by clicking on them!", width / 2, 10);
   noLoop();
 }
 
